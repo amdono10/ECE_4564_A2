f1713324633c5d5c70d4c6417f24717ca4729bb4
Brendan <obren800@vt.edu>
2017-11-06 02:57:06 +0000

    Added checkpoints and a test script

--- repository.py
+++ repository.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 import pika
+import time
 from rmq_params import rmq_params
 
-
 Exchange = rmq_params["exchange"]
-print (Exchange)
+master = rmq_params["master_queue"]
+status = rmq_params["status_queue"]
 
 print ("establishing credentials")
 credentials = pika.PlainCredentials('brendan', 'admin')
@@ -17,23 +18,33 @@ channel = connection.channel()
 queues = []
 for queue in rmq_params["queues"]:
     queues.append(queue)
-    #on startup must purge and unbind the queues
-    channel.queue_purge(queue)
-    channel.queue_unbind(queue)
 
 channel.exchange_declare(exchange=Exchange, exchange_type='direct')
+
+#bind the master queue to the exchange
+channel.queue_declare(queue=master)
+channel.queue_bind(master, Exchange)
+channel.queue_purge(master)
+
+#bind the status queue
+channel.queue_declare(queue=status)
+channel.queue_bind(status, Exchange)
+channel.queue_purge(status)
 
 for q in queues:
     channel.queue_declare(queue=q)
     channel.queue_bind(q, Exchange)
+    channel.queue_bind(master, Exchange, q)
+    channel.queue_purge(q)
 
 
 def callback(ch, method, properties, body):
-    print(" [x] Received %r" % body)
+    print(" [3] Consumed a message with routing key: %s " % method.routing_key)
+    print(" [4] Message: %s" % body)
+    time.sleep(2)
+    print(" [x] Flashing LED Purple")
 
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
+channel.basic_consume(callback,queue=master,no_ack=True)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
